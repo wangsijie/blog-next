4fa6af8279e227b6032b898407a0d96a10ffa261
wangsijie <wangsijie94@gmail.com>
2021-11-17 14:20:24 +0800

    upload-file

---
+++ components/upload-file.tsx
@@ -0,0 +1,71 @@
+import OSS from "ali-oss";
+import { ChangeEventHandler, useState } from "react";
+import CopyToClipboard from "react-copy-to-clipboard";
+import Random from "readableuuid";
+
+interface Props {
+  ak: string;
+  sk: string;
+  bucket: string;
+  endpoint: string;
+  prefix?: string;
+}
+
+const UploadFile = ({ ak, sk, bucket, endpoint, prefix }: Props) => {
+  const [file, setFile] = useState<File>();
+  const [loading, setLoading] = useState(false);
+  const [progress, setProgress] = useState(0);
+  const [url, setUrl] = useState<string>();
+
+  const handleFileChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    if (!e.target.files || !e.target.files[0] || !e.target.value) {
+      return;
+    }
+
+    setFile(e.target.files[0]);
+  };
+
+  const handleUpload = async () => {
+    if (!file) {
+      throw new Error("file is empty");
+    }
+    setLoading(true);
+    const client = new OSS({
+      accessKeyId: ak,
+      accessKeySecret: sk,
+      bucket,
+      endpoint,
+      secure: true,
+    });
+    const result = await client.multipartUpload(
+      `${prefix}${Random()}/${file.name}`,
+      file,
+      {
+        progress(p) {
+          setProgress(Math.floor(p * 100));
+        },
+      }
+    );
+    setUrl(`https://${result.bucket}.${endpoint}/${result.name}`);
+    setLoading(false);
+    setProgress(0);
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={handleFileChange} disabled={loading} />
+      <button onClick={handleUpload} disabled={!file || loading}>
+        {loading ? `${progress}%` : "上传"}
+      </button>
+      <div style={{ marginTop: "10px" }}>
+        {url && (
+          <CopyToClipboard text={encodeURI(url)}>
+            <button>{url}</button>
+          </CopyToClipboard>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default UploadFile;
--- pages/tool.tsx
+++ pages/tool.tsx
@@ -1,11 +1,22 @@
-import type { NextPage } from "next";
+import type { NextPage, NextPageContext } from "next";
 import Head from "next/head";
 import Aes from "../components/aes";
 import Note from "../components/note";
 import RandomString from "../components/random-string";
+import UploadFile from "../components/upload-file";
 import styles from "../styles/tool.module.css";
 
-const Home: NextPage = () => {
+interface Props {
+  oss: {
+    ak: string;
+    sk: string;
+    bucket: string;
+    endpoint: string;
+    prefix?: string;
+  };
+}
+
+const Home: NextPage<Props> = ({ oss }) => {
   return (
     <div className={styles.page}>
       <Head>
@@ -17,8 +28,24 @@ const Home: NextPage = () => {
       <Note />
       <h2>AES</h2>
       <Aes />
+      <h2>FILE</h2>
+      <UploadFile {...oss} />
     </div>
   );
 };
+
+export function getStaticProps() {
+  return {
+    props: {
+      oss: {
+        ak: process.env.TEMP_OSS_AK,
+        sk: process.env.TEMP_OSS_SK,
+        bucket: process.env.TEMP_OSS_BUCKET,
+        endpoint: process.env.TEMP_OSS_ENDPOINT,
+        prefix: process.env.TEMP_OSS_PREFIX,
+      },
+    },
+  };
+}
 
 export default Home;
